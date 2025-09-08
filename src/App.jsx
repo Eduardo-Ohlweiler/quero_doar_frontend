47aac5cf9579ad4c70fb8aa84f5bbbc17f0ecac8
Kauê Gomes <kaue.gomes@ufrgs.br>
2025-09-08 17:26:46 -0300

    feat:Atualiza import do componente ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import { AuthProvider} from "./context/AuthContext";
 import CreateUser from "./pages/CreateUser";
 import CreateDonation from "./pages/CreateDonation";
