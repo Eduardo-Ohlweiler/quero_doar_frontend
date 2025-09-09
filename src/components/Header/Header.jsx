e7d969b3aa92e297cc323bf8e14a1805de0a7739
Kauê Gomes <kaue.gomes@ufrgs.br>
2025-09-09 10:40:39 -0300

    fix: Corrige a constante HEADER_REM_HEIGHT para cálculos de layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -78,4 +78,4 @@ Header.defaultProps = {
 };
 
 export default Header;
-export const HEADER_REM_HEIGHT = 14; // 14rem height for layout calculations
+export const HEADER_REM_HEIGHT = 3.5; // 14rem height for layout calculations
