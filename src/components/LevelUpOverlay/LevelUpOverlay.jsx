521ef79f4143d2caa6d5a4ee0ff0626d65b4d2c5
Kauê Gomes <kaue.gomes@ufrgs.br>
2025-09-13 15:10:28 -0300

    feat: Substitui botão HTML por componente Button no LevelUpOverlay, adiciona elementos de texto

--- src/components/LevelUpOverlay/LevelUpOverlay.jsx
+++ src/components/LevelUpOverlay/LevelUpOverlay.jsx
@@ -2,6 +2,7 @@ import Overlay from "../Overlay/Overlay";
 import LevelAchievement from "../LevelAchievement/LevelAchievement";
 import { useState, useEffect } from "react";
 import confetti from "canvas-confetti";
+import Button from "../Button/Button";
 
 export default function LevelUpOverlay({
     isActive = false,
@@ -52,17 +53,31 @@ export default function LevelUpOverlay({
             zLevel="low"
         >
             <div className="h-[80vh] w-[80vw] flex flex-col items-center justify-center space-y-4 p-4">
-                <button
+                <Button
+                    appearance="ghost"
                     onClick={handleClose}
                     className="absolute right-0 top-0"
                 >
                     Fechar
-                </button>
+                </Button>
+                <span
+                    className="text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-br from-yellow-300 via-yellow-400 to-yellow-300"
+                >
+                    Parabéns! Você subiu de nível!
+                </span>
+                <br/>
                 <LevelAchievement
                     level={level}
                     phrase={phrase}
                     show={isActive}
                 />
+                <br/>
+                <br/>
+                <span
+                    className="text-sm text-black/75"
+                >
+                    Acesse as configurações para desativar notificações de nível
+                </span>
             </div>
         </Overlay>
     );
